d493f3a3b9db5644f9f778405cb2704b681ca3ed
Kendra Tate <kendratate@comcast.net>
2017-04-18 09:34:45 -0600

    Completed and submitted

--- contacts.js
+++ contacts.js
@@ -3,48 +3,73 @@ var path = require('path');
 var fs = require('fs');
 var bodyParser = require('body-parser');
 
-
-
-
 module.exports = function(app){
     var contactsFile = path.join(__dirname, 'contacts.json');
 
     function findRemove(arr, property, item){
-        var i = arr.length;
-
+        var i = arr.length + 1;
         while(i--) {
             if (arr[i]
                 && arr[i].hasOwnProperty(property)
-                && (arguments.length > 2 && arr[i][property] === item )) {
+                && (arr[i][property] === item )) {
                 arr.splice(i, 1);
             }
         }
         return arr;
     }
 
-    app.get('/', function (req, res){
+    app.get(['/','/search'], function (req, res){
         res.type('application/json');
         var readable = fs.createReadStream(contactsFile);
         readable.pipe(res);
     });
 
+    // array filters items based on search criteria (query)
+    function filterItems(contactarray, query) {
+        return contactarray.filter(function(el){
+            return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+        })
+    }
+
+    app.get('/search/:searchterm', function (req, res){
+        res.type('application/json');
+        var readdata = JSON.parse(fs.readFileSync(contactsFile, 'utf8'));
+        //console.log(readdata);
+        console.log(req.body);
+        var jsonobject = {contacts: []};
+
+        var i = readdata.contacts.length + 1;
+        while(i--) {
+            if (readdata.contacts[i]
+                && ((readdata.contacts[i].firstname.toLowerCase().indexOf(req.params.searchterm.toLowerCase()) > -1)
+                || (readdata.contacts[i].lastname.toLowerCase().indexOf(req.params.searchterm.toLowerCase()) > -1)
+                || (readdata.contacts[i].phone.toLowerCase().indexOf(req.params.searchterm.toLowerCase()) > -1)
+                )) {
+                jsonobject.contacts.push(readdata.contacts[i]);
+            }
+        }
+        console.log(jsonobject);
+        readdata.contacts = jsonobject;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(JSON.stringify(jsonobject));
+    });
+
+
     app.delete('/:id', function(req,res) {
         console.log("in delete");
         var readdata = JSON.parse(fs.readFileSync(contactsFile, 'utf8'));
-        var data = findRemove(readdata, 'id', req.params.id);
-        console.log(data);
+        var data = findRemove(readdata.contacts, 'id', req.params.id);
 
-        //fs.writeFileSync(contactsFile, JSON.stringify(data,null,2));
-
-        fs.writeFile(contactsFile, JSON.stringify(data,null,2), function(err){
+        readdata.contacts = data;
+        fs.writeFile(contactsFile, JSON.stringify(readdata,null,2), function(err){
             if(err) {
                 console.log(err);
                 return;
             }
-            res.end(JSON.stringify("Item Deleted"));
+            res.end(JSON.stringify(readdata));
            console.log('writing to ' + contactsFile);
         });
-        return data;
+        // return data;
     });
 
     app.get('/sorta', function(req, res){
@@ -109,5 +134,31 @@ module.exports = function(app){
             }
             res.end(JSON.stringify(readdata));
         });
-     })
+    })
+
+    app.post('/:id', function(req, res){
+        var readdata = JSON.parse(fs.readFileSync(contactsFile, 'utf8'));
+        //console.log(readdata);
+        console.log(req.body);
+
+        var i = readdata.contacts.length + 1;
+        while(i--) {
+            if (readdata.contacts[i]
+                && (readdata.contacts[i].id === req.body.id )) {
+                readdata.contacts[i].firstname = req.body.firstname;
+                readdata.contacts[i].lastname = req.body.lastname;
+                readdata.contacts[i].phone = req.body.phone;
+            }
+            }
+        console.log(readdata);
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.writeFile(contactsFile, JSON.stringify(readdata,null,2), function(err){
+            if(err) {
+                console.log(err);
+                return;
+            }
+            res.end(JSON.stringify(readdata));
+        });
+    })
 };
